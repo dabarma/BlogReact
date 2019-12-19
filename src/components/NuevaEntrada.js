bf5a26f8cbeacf613af0fb906a46e75a0d9bb908
David B <dabarma@gmail.com>
2019-12-19 13:48:58 +0100

    validacion formulario

--- src/components/NuevaEntrada.js
+++ src/components/NuevaEntrada.js
@@ -31,7 +31,7 @@ class NuevaEntrada extends React.Component{
         {
             return(
                 <div stylename="text-align:left;">
-                    <form>
+                    <form onSubmit={(e) => { e.preventDefault(); this.props.data(this.state)}}>
                         <h2>Nueva entrada</h2>
                         <div className="form-group">
                             <label>Email:</label>
@@ -63,7 +63,7 @@ class NuevaEntrada extends React.Component{
                                 } }
                             />
                         </div>
-                        <button type="button" className="btn btn-light" onClick={() => this.props.data(this.state)}>Guardar entrada</button>
+                        <input type="submit" className="btn btn-light" value="Guardar entrada"></input>
                         <button className="btn btn-light" onClick={() => this.props.cancelar()}>Cancelar</button>
                     </form>                   
                 </div>
