3d52a04b1cb64ab726a528911772c05eb54fabe6
David B <dabarma@gmail.com>
2019-12-23 12:47:59 +0100

    Creado service mock

--- src/components/EntradaDetalle.js
+++ src/components/EntradaDetalle.js
@@ -1,12 +1,53 @@
 import React from 'react';
+import ServiceApi from '../services/ServiceApi.js';
+import { Link } from 'react-router-dom';
 
 class EntradaDetalle extends React.Component{
 
+    constructor(props){
+
+        super(props);
+        this.serviceApi = ServiceApi.getInstance();  
+        
+        this.state = {
+            entrada: null,
+            loaded: false
+        };
+    }
+
+    componentDidMount(){
+
+        const id = this.props.match.params.id;
+        console.log(id);
+        this.serviceApi.getEntrada(id).then(entrada => {
+
+            console.log(entrada);
+            this.setState({
+              entrada: entrada,
+              loaded: true
+            });
+    
+          });
+    }
+
     render(){
-        console.log(this.props);
-        return (
-        <p>{this.props.match.params.id}</p>
-        );
+        
+        if(this.state.loaded)
+        {
+            return (
+                <div className="detalleEntrada">
+                    <h1>{this.state.entrada.titulo}</h1>
+                    <span className="email">{this.state.entrada.email}</span>
+                    <div className="textoEntrada" dangerouslySetInnerHTML={{ __html: this.state.entrada.texto }}>
+
+                    </div>
+                    <div className="centrado">
+                        <Link className="btn btn-light" to="/">Volver</Link>
+                    </div>           
+                </div>
+                );
+        }
+        else return(<div></div>);
     }
 }
 
---
+++ src/services/ServiceApi.js
@@ -0,0 +1,70 @@
+class ServiceApi{
+
+    static myInstance = null;
+
+    static getInstance() {
+
+        if (ServiceApi.myInstance == null) {
+            ServiceApi.myInstance = new ServiceApi();
+        }
+
+        return this.myInstance;
+    }
+
+    constructor(){
+
+        this.LastId = 4;
+        this.entradas = [
+            { 
+              id: 1,
+              email: 'dabarma@gmail.com',
+              titulo: '¿Por qué JSX?',
+              texto: '<p>React acepta el hecho de que la lógica de renderizado está intrínsecamente unida a la lógica de la interfaz de usuario: cómo se manejan los eventos, cómo cambia el estado con el tiempo y cómo se preparan los datos para su visualización.</p><p>En lugar de separar artificialmente tecnologías poniendo el maquetado y la lógica en archivos separados, React separa intereses con unidades ligeramente acopladas llamadas “componentes” que contienen ambas. Volveremos a los componentes en otra sección, pero si aún no te sientes cómodo maquetando en JS, esta charla podría convencerte de lo contrario.</p><p>React no requiere usar JSX, pero la mayoría de la gente lo encuentra útil como ayuda visual cuando trabajan con interfaz de usuario dentro del código Javascript. Esto también permite que React muestre mensajes de error o advertencia más útiles.</p><p>Con eso fuera del camino, ¡empecemos!</p>'
+            },
+            { 
+              id: 2,
+              email: 'dabarma@gmail.com',
+              titulo: 'React solo actualiza lo que es necesario',
+              texto: '<p>React DOM compara el elemento y su hijos con el elemento anterior, y solo aplica las actualizaciones del DOM que son necesarias para que el DOM esté en el estado deseado.</p>'
+            },
+            { 
+              id: 3,
+              email: 'dabarma@gmail.com',
+              titulo: 'Los datos fluyen hacia abajo',
+              texto: '<p>Ni los componentes padres o hijos pueden saber si un determinado componente tiene o no tiene estado y no les debería importar si se define como una función o una clase.</p><p>Por eso es que el estado a menudo se le denomina local o encapsulado. No es accesible desde otro componente excepto de aquel que lo posee y lo asigna.</p>'
+            },
+            { 
+              id: 4,
+              email: 'dabarma@gmail.com',
+              titulo: 'Suposiciones del nivel de conocimiento',
+              texto: '<p>React es una biblioteca Javascript, y vamos a asumir que tienes un entendimiento básico del lenguaje Javascript. Si no te sientes muy seguro, te recomendamos hacer un tutorial de Javascript para comprobar tu nivel de conocimiento y permitirte seguir adelante con esta guía sin perderte. Te debería tomar entre 30 minutos y una hora, pero como resultado, no tendrás que sentir que estás aprendiendo React y JavaScript al mismo tiempo.</p>'
+            },
+      
+          ];
+    }
+
+    async getEntradas(){
+        return Promise.resolve(this.entradas);
+    }
+
+    async getEntrada(id){
+
+        let entrada=this.entradas.find(p => p.id === Number.parseInt(id));
+        
+        return Promise.resolve(entrada);
+    }
+
+    async insertEntrada(entrada){
+
+        this.LastId++;
+        entrada.id = this.LastId;
+
+        let entradas = this.entradas.slice();
+        this.entradas = [entrada].concat(entradas);
+
+        return entrada;
+    }
+
+}
+
+export default ServiceApi;
